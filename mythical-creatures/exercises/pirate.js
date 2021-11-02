9fe8abfc3234c180040c2f316a049e1260d76037
Rosa The Dev <richardjrosaserrano@gmail.com>
2021-11-01 21:31:04 -0400

    complete pirate

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,28 @@
+class Pirate {
+    constructor(name, job) {
+        this.name = name;
+        this.job = job || 'scallywag'; 
+        this.cursed = false;
+        this.booty = 0;
+    }
+
+    robShip() {
+        if(this.booty <= 400) {
+            this.booty += 100
+        } else {
+        this.cursed = true;
+        return 'ARG! I\'ve been cursed!'
+        }
+        return 'YAARRR!';
+    }
+
+    liftCurse() {
+        if(this.booty < 500) {
+            return 'You don\'t need to lift a curse!'
+        }
+            this.booty -= 300;
+            this.cursed = false;
+            return 'Your curse has been lifted!'
+    }
+}
+module.exports = Pirate;
