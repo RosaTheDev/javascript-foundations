bfeb6576a6d6a1d941d7d8ee89eb1ff37db501bd
Rosa The Dev <richardjrosaserrano@gmail.com>
2021-11-04 15:54:17 -0400

    Completed medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,24 @@
 var Person = require('./person');
 var Statue = require('./statue');
+class Medusa {
+    constructor(name) {
+       
+        this.name = name;
+        this.statues = [];
+    }
+
+    gazeAtVictim(victim) {
+        var newStatue = new Statue(victim.name);
+        if(this.statues.length < 3) {
+            this.statues.push(newStatue)
+        } else if(this.statues.length >= 3){
+            var freedPerson = this.statues.shift();
+            this.statues.push(newStatue);
+            var newPerson = new Person(freedPerson.name);
+            newPerson.mood = 'relieved'
+            return newPerson
+        }
+    }
+}
+
+module.exports = Medusa;
