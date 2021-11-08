f1b9b3270dc4b92386666bed441fd6919e316fd5
Richard J Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-07 20:51:32 -0500

    Complete roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,26 @@
 class Roadrace {
+    constructor(race) {
+        this.name = race.title;
+        this.location = race.city;
+        this.participants = [];
+    }
 
+    setDistance(distance) {
+        this.distance = distance;
+        return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`;
+    }
+
+    registerParticipants(racer) {
+        this.participants.push(racer);
+    }
+    
+    completeRace() {
+        for(var i = 0; i < this.participants.length; i++) {
+            this.participants[i].completedRaces.push(this.name);
+            this.participants[i].milesRun = this.distance
+        }
+
+    }
 }
 
 module.exports = Roadrace;
