b122986495b32cab26ad3ab2e8655ad52aa6ff95
Richard J Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-07 19:44:58 -0500

    Completed Crafting files

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,21 @@
-class Craft {
+const Material = require("./material");
 
+Material
+class Craft {
+    constructor(craft) {
+        this.name = craft.type;
+        this.materials = craft.materials;
+        this.completed = false;
+    }
+
+    completeCraft() {
+        this.completed = true;
+        return 'All done! It looks great!';
+    }
+
+    calculateProjectTotal() {
+       return  this.materials[0].price * this.materials[0].amount + this.materials[1].price * this.materials[1].amount
+    }
 }
 
 module.exports = Craft;
