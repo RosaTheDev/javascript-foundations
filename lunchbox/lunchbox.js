4837dccb9653f11e6ded01df1dcd368d5384f641
Rosa The Dev <richardjrosaserrano@gmail.com>
2021-11-09 00:50:19 -0500

    Complete lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
+    constructor(lunchBox) {
+        this.owner = lunchBox.owner;
+        this.material = lunchBox.madeOf;
+        this.shape = lunchBox.shape;
+        this.color = lunchBox.color;
+        this.snacks = [];
+        this.healthySnack = [];
+    }
 
+    acquireSnack(fruitSnack) {
+        this.snacks.push(fruitSnack);
+        this.fruitSnack = fruitSnack;
+        fruitSnack.isInLunchBox = true;
+        if(this.fruitSnack.type.includes('fruit') === true || this.fruitSnack.type.includes('Fruit') === true) {
+                this.healthySnack.push(this.fruitSnack.type);
+            }
+    }
+
+    findHealthySnacks() {
+        return this.healthySnack;
+            
+    }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,25 @@
 class Snack {
+    constructor(snackName) {
+        this.type = snackName;
+        this.deliciousLevel = 'extra';
+        this.amount = 100;
+        this.isInLunchBox = false;
+        this.cuttingItClose = false;
+    }
 
+    getEaten() {
+        this.amount -= 10;
+        if(this.amount <= 20) {
+            this.cuttingItClose = true;
+        }
+    }
+
+    checkForHealthy() {
+        if(this.type.includes('fruit') || this.type.includes('Fruit')) {
+            return true;
+        } 
+        return false;
+    }
 }
 
 module.exports = Snack;
